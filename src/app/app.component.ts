d7afd1d29a39f204d43ac2bc9b15fd9edd07d119
Alessandro Caggiano <ale.caggiano@hispeed.ch>
2024-07-28 15:28:04 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule, DatePipe} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {CdkOverlayOrigin} from "@angular/cdk/overlay";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CdkOverlayOrigin],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'fashion-app';
+  todaysDate = new Date();
+  protected readonly DatePipe = DatePipe;
+
 }
